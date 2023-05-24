fbd87acff98cc76dc41e1712763124d19b7d5c04
Brandon Shin <bmshin02@gmail.com>
2023-05-24 11:37:43 -0700

    Update ParticleCollsion.cs

--- Assets/Script/ParticleCollsion.cs
+++ Assets/Script/ParticleCollsion.cs
@@ -18,20 +18,22 @@ public class ParticleCollsion : MonoBehaviour
         //Debug.Log("Other collider tag is:" + otherCollider.tag);
 
         //Made it to detect if individual particles clash with each other.
-        //NOTE: I think because this script is attached to all NO2 molecules, including the generated ones,
+        //NOTE: I think because this script is attached to all NO2 molecules, incl uding the generated ones,
         //the code below runs for each one, thus 2 messages are printed for one collision,
         //thus i think two game objects also call the Destroy method on itself and the other.
         //This isn't a problem now since we just destroy them, but it might be later when we try to generate new N204 molecules
         //We need to make sure we generate 1 and not 2 new ones
-        if (thisCollider.CompareTag("Nitrogen") && otherCollider.CompareTag("Nitrogen"))
+
+        //thisCollider.CompareTag("Nitrogen") && otherCollider.CompareTag("Nitrogen")
+
+        if (collision.collider.gameObject.CompareTag("Nitrogen"))
         {
             Debug.LogWarning("Nitrogens Hit!");
             Destroy(collision.gameObject);
             Destroy(collision.gameObject);
 
+            
             particleGen.GetComponent<ParticleGeneration>().InstantiateGameObjects(GameObject.Find("Molecule"));
-
-
         }
 
         if (thisCollider.CompareTag("Oxygen") && otherCollider.CompareTag("Oxygen"))
