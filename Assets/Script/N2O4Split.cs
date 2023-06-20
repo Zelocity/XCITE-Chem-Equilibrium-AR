f00a31625b82a2aea783a955fa40451fa1c3b885
Brandon Shin <bmshin02@gmail.com>
2023-06-20 16:30:31 -0700

    Fixes #4

--- Assets/Script/N2O4Split.cs
+++ Assets/Script/N2O4Split.cs
@@ -20,25 +20,28 @@ public class N2O4Split : MonoBehaviour
     // Start is called before the first frame update
     void Update()
     {
+        currList = particleGen.GetComponent<ParticleGeneration>().GetN2O4List();
+        listSize = currList.Count;
+        timer += Time.deltaTime;
 
-        //currList = particleGen.GetComponent<ParticleGeneration>().GetN2O4List();
-        //listSize = currList.Count;
-        //timer += Time.deltaTime;
+        if (timer > 10f && listSize > 0)
+        {
 
-        //if (timer > 5f && listSize > 0)
-        //{
-        //    // Debug.Log("5 SECONDS PASSED. About to delete");
-        //    particleGen.GetComponent<ParticleGeneration>().DestroyGameObjects("N2O4");
-        //    timer = 0f;
-        //}
+            Debug.Log("5 SECONDS PASSED. About to delete");
+            particleGen.GetComponent<ParticleGeneration>().DestroyGameObjects("N2O4");
+            particleGen.GetComponent<ParticleGeneration>().InstantiateGameObjects(GameObject.Find("NO2"), 2, new Vector3(0, 0, 0));
+            timer = 0f;
+            UIScript.numNO2 += 2; 
+            UIScript.numN2O4--;
+        }
 
-        /*StartCoroutine(waiter());*/
+        //StartCoroutine(waiter());
     }
 
     // Update is called once per frame
-/*    IEnumerator waiter()
+    IEnumerator waiter()
     {
-        yield return new WaitForSeconds(5);
-        Object.Destroy(this.gameObject);
-    }*/
+       yield return new WaitForSeconds(5);
+       Object.Destroy(this.gameObject);
+    }
 }
